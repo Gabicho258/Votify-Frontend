f29fa243807da69f9aa5cb91ed16eb83f1af0221
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-06-27 22:14:05 -0500

    add temporal files for creating protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/User/Landing/Landing";
+// import { Login } from "./pages/User/Login/Login";
+
+// Test
+// import {
+//   HomeProcessAdmin,
+//   HomeSysAdmin,
+//   HomeUser,
+//   Landing,
+//   Votar,
+// } from "./pages/test";
+// import { useState } from "react";
+// import { ProtectedRoute } from "./routes/ProtectedRoute";
+// interface UserTemp {
+//   id: number;
+//   name: string;
+//   role: string | "user" | "process" | "system";
+// }
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Test setup
+  // const [user, setuser] = useState<UserTemp | null>(null);
+
+  // const login = (role: string) => {
+  //   setuser({
+  //     id: 1,
+  //     name: "Juanito Perez",
+  //     role,
+  //   });
+  //   console.log(role);
+  // };
+  // const logout = () => {
+  //   setuser(null);
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Testing routes to test pages
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
+    // Protected routes - Official
+    // <BrowserRouter>
+    //   <Navigation />
+    //   {user ? (
+    //     <button onClick={logout}>Logout</button>
+    //   ) : (
+    //     <>
+    //       <button onClick={() => login("user")}>Login as User</button>
+    //       <button onClick={() => login("process")}>
+    //         Login as Process Admin
+    //       </button>
+    //       <button onClick={() => login("system")}>Login as System Admin</button>
+    //     </>
+    //   )}
+    //   <Routes>
+    //     <Route index element={<Landing />} />
+    //     <Route path="/landing" element={<Landing />} />
+    //     <Route
+    //       element={
+    //         <ProtectedRoute
+    //           children={null}
+    //           isAuthenticated={!!user && user.role == "user"}
+    //         />
+    //       }
+    //     >
+    //       <Route path="home-user" element={<HomeUser />} />
+    //       <Route path="votar" element={<Votar />} />
+    //     </Route>
+    //     <Route
+    //       path="/process"
+    //       element={
+    //         <ProtectedRoute isAuthenticated={!!user && user.role == "process"}>
+    //           <HomeProcessAdmin />
+    //         </ProtectedRoute>
+    //       }
+    //     />
+    //     <Route
+    //       path="/system"
+    //       element={
+    //         <ProtectedRoute isAuthenticated={!!user && user.role == "system"}>
+    //           <HomeSysAdmin />
+    //         </ProtectedRoute>
+    //       }
+    //     />
+    //   </Routes>
+    // </BrowserRouter>
+  );
 }
+// const Navigation = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/landing">Landing</Link>
+//         </li>
+//         <li>
+//           <Link to="/home-user">Home User</Link>
+//         </li>
+//         <li>
+//           <Link to="/votar">Votar</Link>
+//         </li>
+//         <li>
+//           <Link to="/process">Process Admin</Link>
+//         </li>
+//         <li>
+//           <Link to="/system">System Admin</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
 
-export default App
+export default App;
---
+++ src/pages/test.tsx
@@ -0,0 +1,15 @@
+export const Landing = () => {
+  return <h1>Landing page - Everyone</h1>;
+};
+export const HomeUser = () => {
+  return <h1>Home page - User </h1>;
+};
+export const Votar = () => {
+  return <h1>Votar page - User</h1>;
+};
+export const HomeSysAdmin = () => {
+  return <h1>Home page - SysAdmin</h1>;
+};
+export const HomeProcessAdmin = () => {
+  return <h1>Home page - Process Admin</h1>;
+};
