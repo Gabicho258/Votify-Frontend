b183c7cdf75b1c581ac72e5a657e92c13f3a3e41
Gabriel <gmamanican@unsa.edu.pe>
2024-07-02 19:33:10 -0500

    add new flow login

--- src/pages/User/Login/Login.tsx
+++ src/pages/User/Login/Login.tsx
@@ -7,54 +7,68 @@ import axios from "axios";
 
 export const Login = () => {
   const handleGoogleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      console.log(tokenResponse);
-      const userInfo = await axios.get(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
-      );
+    flow: "auth-code",
+    onSuccess: async (codeResponse) => {
+      console.log(codeResponse);
+      const tokens = await axios.post("http://localhost:5000/auth/google", {
+        code: codeResponse.code,
+      });
 
-      console.log(userInfo);
+      console.log(tokens);
     },
     onError: (errorResponse) => console.log(errorResponse),
   });
 
+  // Implicit flow
+  // const handleGoogleLogin = useGoogleLogin({
+  //   onSuccess: async (tokenResponse) => {
+  //     console.log(tokenResponse);
+  //     const userInfo = await axios.get(
+  //       "https://www.googleapis.com/oauth2/v3/userinfo",
+  //       { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
+  //     );
+
+  //     console.log(userInfo);
+  //   },
+  //   onError: (errorResponse) => console.log(errorResponse),
+  // });
+
   // const handleGoogleLogin = () => {};
   return (
-    <div className="container">
-      <div className="container__left">
-        <div className="container__left-info">
-          <div className="container__left-info-image">
+    <div className="containerLogin">
+      <div className="containerLogin__left">
+        <div className="containerLogin__left-info">
+          <div className="containerLogin__left-info-image">
             <img
               src="https://raw.githubusercontent.com/Gabicho258/votify-frontend/master/src/assets/logo_clean_zoom.png"
               alt="logo-votify"
             />
           </div>
-          <div className="container__left-info-welcome">
+          <div className="containerLogin__left-info-welcome">
             BIENVENIDOS A <br />
             VOTIFY
           </div>
-          <div className="container__left-info-slogan">
+          <div className="containerLogin__left-info-slogan">
             Centro de votación seguro
             <br /> donde tu voto es confidencial
           </div>
         </div>
-        <div className="container__left-login">
-          <div className="container__left-login-message">
+        <div className="containerLogin__left-login">
+          <div className="containerLogin__left-login-message">
             Acceda con su cuenta
           </div>
           <Button
             variant="outlined"
             tabIndex={-1}
             startIcon={<GoogleIcon />}
-            className="container__left-login-button"
+            className="containerLogin__left-login-button"
             onClick={() => handleGoogleLogin()}
           >
             Acceder con Google
           </Button>
         </div>
       </div>
-      <div className="container__right" />
+      <div className="containerLogin__right" />
     </div>
   );
 };
