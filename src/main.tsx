fc775f78150e768e32d562118afd3de0cb348248
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-07 23:56:45 -0500

    add redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from "./App.tsx";
 import "./_index.scss";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <GoogleOAuthProvider clientId="799776415523-fio97vuli22c1jdc474jg2e3fnjuf9r3.apps.googleusercontent.com">
-        <App />
-      </GoogleOAuthProvider>
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <GoogleOAuthProvider clientId="799776415523-fio97vuli22c1jdc474jg2e3fnjuf9r3.apps.googleusercontent.com">
+          <App />
+        </GoogleOAuthProvider>
+      </StyledEngineProvider>
+    </Provider>
   </React.StrictMode>
 );
