9db045b0114d5ff4dfa44491312b0bf308aa41a5
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-20 23:30:15 -0500

    add connection with backend

--- src/pages/User/InProgressProcessInfo/InProgressProcessInfo.tsx
+++ src/pages/User/InProgressProcessInfo/InProgressProcessInfo.tsx
@@ -2,23 +2,47 @@ import "./_InProgressProcessInfo.scss";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import { useNavigate } from "react-router-dom";
-import { IElectionProcess } from "../../../interfaces";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  useGetCredentialsByUserIdQuery,
+  useGetProcessByIdQuery,
+  useGetUserByIdQuery,
+} from "../../../app/votify.api";
+import { useEffect } from "react";
 
 export const InProgressProcessInfo = () => {
+  const { process_id } = useParams();
+
   const navigate = useNavigate();
+  const { data: process } = useGetProcessByIdQuery(process_id || "");
 
-  const process: IElectionProcess = {
-    _id: "1a2b3c4d5e6f7g8h9i0j",
-    user_id: "user123",
-    is_owner: true,
-    title: "Presidential Election 2024",
-    admin_status: "approved",
-    process_status: "active",
-    start_date: "2024-08-01T00:00:00.000Z",
-    end_date: "2024-11-01T23:59:59.000Z",
+  const user_id = localStorage.getItem("voter_id") || "";
+  const { data: userCredentials, refetch: refetchCredentials } =
+    useGetCredentialsByUserIdQuery(user_id);
+  const { data: currentUser } = useGetUserByIdQuery(user_id);
+  const credentialForCurrentProcess = userCredentials?.find(
+    (credential) => credential.process_id === process_id
+  );
+  const handleContinue = () => {
+    if (!credentialForCurrentProcess) {
+      alert("No tienes acceso para este proceso.");
+      navigate("/hub", { replace: true });
+      return;
+    }
+
+    navigate("/credential", {
+      state: {
+        process_id,
+        dni: currentUser?.dni,
+        password: credentialForCurrentProcess?.password,
+        was_used: credentialForCurrentProcess?.was_used,
+        credential_id: credentialForCurrentProcess?._id,
+      },
+    });
   };
-
+  useEffect(() => {
+    refetchCredentials();
+  }, []);
   return (
     <div className="containerInProgressProcessInfo">
       <div
@@ -31,26 +55,26 @@ export const InProgressProcessInfo = () => {
       <div className="containerInProgressProcessInfo__content">
         <div className="containerInProgressProcessInfo__content-box">
           <div className="containerInProgressProcessInfo__content-box-title">
-            {process.title}
+            {process?.title}
           </div>
           <div className="containerInProgressProcessInfo__content-box-info">
             <div className="containerInProgressProcessInfo__content-box-info-date">
               <DateRangeIcon className="containerInProgressProcessInfo__content-box-info-date-icon" />
               <div className="containerInProgressProcessInfo__content-box-info-date-text">
-                {new Date(process.start_date).toLocaleDateString() +
+                {new Date(process?.start_date || "").toLocaleDateString() +
                   " - " +
-                  new Date(process.end_date).toLocaleDateString()}
+                  new Date(process?.end_date || "").toLocaleDateString()}
               </div>
             </div>
             <div className="containerInProgressProcessInfo__content-box-info-time">
               <AccessTimeIcon className="containerInProgressProcessInfo__content-box-info-time-icon" />
               <div className="containerInProgressProcessInfo__content-box-info-time-text">
-                {new Date(process.start_date).toLocaleTimeString([], {
+                {new Date(process?.start_date || "").toLocaleTimeString([], {
                   hour: "2-digit",
                   minute: "2-digit",
                 }) +
                   " - " +
-                  new Date(process.end_date).toLocaleTimeString([], {
+                  new Date(process?.end_date || "").toLocaleTimeString([], {
                     hour: "2-digit",
                     minute: "2-digit",
                   })}
@@ -75,7 +99,10 @@ export const InProgressProcessInfo = () => {
             </ul>
           </div>
           <div className="containerInProgressProcessInfo__content-box-button">
-            <button className="containerInProgressProcessInfo__content-box-button-continue">
+            <button
+              className="containerInProgressProcessInfo__content-box-button-continue"
+              onClick={handleContinue}
+            >
               Ir a votar
             </button>
           </div>
