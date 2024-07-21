89d2e92e08b8d3fb18ed2a1dfe1a3be999adf50e
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-20 23:30:22 -0500

    add connection with backend

--- src/pages/User/Credential/Credential.tsx
+++ src/pages/User/Credential/Credential.tsx
@@ -3,6 +3,8 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 import "./_Credential.scss";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useGetProcessByIdQuery } from "../../../app/votify.api";
 
 type CredentialInputs = {
   dni: number;
@@ -10,25 +12,44 @@ type CredentialInputs = {
 };
 
 export const Credential = () => {
+  const navigate = useNavigate();
+  const { state } = useLocation();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<CredentialInputs>();
+  const { data: currentProcess } = useGetProcessByIdQuery(state.process_id);
+  console.log(state);
   const onSubmit: SubmitHandler<CredentialInputs> = async (data) => {
+    if (state.was_used) {
+      alert("Usted ya realizó un voto");
+      return;
+    }
     // submit code
 
-    console.log(data);
+    if (state.dni === data.dni && state.password === data.password) {
+      navigate("/process-help", {
+        state: {
+          process_id: state.process_id,
+          credential_id: state.credential_id,
+        },
+        replace: true,
+      });
+    } else {
+      alert("DNI o contraseña incorrectos");
+    }
   };
+  if (state === null) return <Navigate to={"/hub"} replace={true} />;
   return (
     <div className="credentialContainer">
-      <div className="credentialContainer__back">
+      <div className="credentialContainer__back" onClick={() => navigate(-1)}>
         <ArrowBackIcon className="credentialContainer__back-icon" />
         <span>Volver</span>
       </div>
       <div className="credentialContainer__content">
         <h2 className="credentialContainer__content-title">
-          Ingrese sus credenciales para votar en{" PROCESOSS 1"}
+          {`Ingrese sus credenciales para votar en ${currentProcess?.title}`}
         </h2>
         <form
           className="credentialContainer__content-form"
