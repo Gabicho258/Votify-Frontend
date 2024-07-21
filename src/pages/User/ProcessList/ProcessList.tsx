1b82149438fe0632bf3138a55510eb41fb07ae3d
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-20 23:30:42 -0500

    handle confirm vote

--- src/pages/User/ProcessList/ProcessList.tsx
+++ src/pages/User/ProcessList/ProcessList.tsx
@@ -1,72 +1,117 @@
-import './_ProcessList.scss';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import { Button } from '@mui/material';
-import { ICandidate, IList } from '../../../interfaces';
-import { useState } from 'react';
-import { ProcessListItem } from '../../../components/ProcessListItem/ProcessListItem';
+import "./_ProcessList.scss";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { Button } from "@mui/material";
+import { ICandidate, IList } from "../../../interfaces";
+import { useEffect, useState } from "react";
+import { ProcessListItem } from "../../../components/ProcessListItem/ProcessListItem";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  useGetCandidatesByListIdQuery,
+  useGetListsByProcessIdQuery,
+} from "../../../app/votify.api";
 
 export const ProcessList = () => {
+  const { state } = useLocation();
   const [isLastList, setIsLastList] = useState(false);
-  const [selectedCandidateId, setSelectedCandidateId] = useState<string | null>(
+  const [selectedCandidate, setSelectedCandidate] = useState<ICandidate | null>(
     null
   );
 
-  const handleSelectCandidate = (id: string) => {
-    setSelectedCandidateId(id);
+  const handleSelectCandidate = (candidate: ICandidate) => {
+    setSelectedCandidate(candidate);
   };
+  const [candidatesSelected, setCandidatesSelected] = useState<ICandidate[]>(
+    []
+  );
+  const navigate = useNavigate();
+  const { data: listsOfProcess } = useGetListsByProcessIdQuery(
+    state.process._id
+  );
+  const [index, setIndex] = useState(0);
 
-  const list: IList = {
-    _id: '5e6f7g8h9i0j1a2b3c4d',
-    process_id: 'process202',
-    title: 'Candidato a presidente',
+  const handleNextList = () => {
+    if (selectedCandidate === null) {
+      alert("Debe seleccionar un candidato primero.");
+      return;
+    }
+    setCandidatesSelected([...candidatesSelected, selectedCandidate]);
+    setIndex((prevIndex) => prevIndex + 1);
+    setSelectedCandidate(null);
+    if (index === (listsOfProcess?.length || 0) - 2) {
+      setIsLastList(true);
+      return;
+    }
   };
+  useEffect(() => {}, []);
+  const handleNextSummary = () => {
+    navigate("/votes-summary", {
+      state: {
+        candidates: [...candidatesSelected, selectedCandidate],
+        process: state.process,
+        credential_id: state.credential_id,
+      },
+    });
+  };
+  const currentList = Array.from(listsOfProcess || [])[index];
+  useEffect(() => {
+    if (state && state.candidates) {
+      setSelectedCandidate(state.candidates[index]);
+    }
+  }, [state, index]);
+  // console.log(selectedCandidate);
 
-  const candidates: ICandidate[] = [
-    {
-      _id: 'c1a2b3c4d5e6f7g8h9i0j',
-      list_id: '5e6f7g8h9i0j1a2b3c4d',
-      candidate_name: 'Alice Johnson',
-      photo_url:
-        'https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg',
-      organization_name: 'Tech Innovators',
-      logo_url:
-        'https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png',
-      valid_votes: 1500,
-    },
-    {
-      _id: 'c1a2b3c4d5e6f7g8h9i0a',
-      list_id: '5e6f7g8h9i0j1a2b3c4d',
-      candidate_name: 'Alice Johnson',
-      photo_url:
-        'https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg',
-      organization_name: 'Tech Innovators',
-      logo_url:
-        'https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png',
-      valid_votes: 1500,
-    },
-    {
-      _id: 'c1a2b3c4d5e6f7g8h9i0w',
-      list_id: '5e6f7g8h9i0j1a2b3c4d',
-      candidate_name: 'Alice Johnson',
-      photo_url:
-        'https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg',
-      organization_name: 'Tech Innovators',
-      logo_url:
-        'https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png',
-      valid_votes: 1500,
-    },
-    {
-      _id: 'algunidparanull',
-      list_id: '5e6f7g8h9i0j1a2b3c4d',
-      candidate_name: 'null',
-      photo_url:
-        'https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg',
-      organization_name: 'Tech Innovators',
-      logo_url:
-        'https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png',
-      valid_votes: 1500,
-    },
-  ];
+  // const list: IList = {
+  //   _id: "5e6f7g8h9i0j1a2b3c4d",
+  //   process_id: "process202",
+  //   title: "Candidato a presidente",
+  // };
+
+  // const candidates: ICandidate[] = [
+  //   {
+  //     _id: "c1a2b3c4d5e6f7g8h9i0j",
+  //     list_id: "5e6f7g8h9i0j1a2b3c4d",
+  //     candidate_name: "Alice Johnson",
+  //     photo_url:
+  //       "https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg",
+  //     organization_name: "Tech Innovators",
+  //     logo_url:
+  //       "https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png",
+  //     valid_votes: 1500,
+  //   },
+  //   {
+  //     _id: "c1a2b3c4d5e6f7g8h9i0a",
+  //     list_id: "5e6f7g8h9i0j1a2b3c4d",
+  //     candidate_name: "Alice Johnson",
+  //     photo_url:
+  //       "https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg",
+  //     organization_name: "Tech Innovators",
+  //     logo_url:
+  //       "https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png",
+  //     valid_votes: 1500,
+  //   },
+  //   {
+  //     _id: "c1a2b3c4d5e6f7g8h9i0w",
+  //     list_id: "5e6f7g8h9i0j1a2b3c4d",
+  //     candidate_name: "Alice Johnson",
+  //     photo_url:
+  //       "https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg",
+  //     organization_name: "Tech Innovators",
+  //     logo_url:
+  //       "https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png",
+  //     valid_votes: 1500,
+  //   },
+  //   {
+  //     _id: "algunidparanull",
+  //     list_id: "5e6f7g8h9i0j1a2b3c4d",
+  //     candidate_name: "null",
+  //     photo_url:
+  //       "https://i.pinimg.com/originals/c7/a9/40/c7a9408babdc2852a48191ab83a5944b.jpg",
+  //     organization_name: "Tech Innovators",
+  //     logo_url:
+  //       "https://png.pngtree.com/png-clipart/20221214/ourlarge/pngtree-shovel-clipart-png-image_6522991.png",
+  //     valid_votes: 1500,
+  //   },
+  // ];
   return (
     <div className="containerProcessList">
       <div className="containerProcessList__timer">
@@ -75,23 +120,19 @@ export const ProcessList = () => {
           <div className="containerProcessList__timer-box-time">10:00</div>
         </div>
       </div>
-      <div className="containerProcessList__title">{list.title}</div>
-      <div className="containerProcessList__candidates">
-        {candidates.map((candidate) => (
-          <ProcessListItem
-            key={candidate._id}
-            candidate={candidate}
-            isSelected={candidate._id === selectedCandidateId}
-            onSelect={handleSelectCandidate}
-          />
-        ))}
-      </div>
+
+      <Options
+        list={currentList}
+        handleSelectCandidate={handleSelectCandidate}
+        selectedCandidate={selectedCandidate}
+      />
+
       <div className="containerProcessList__button">
         {isLastList ? (
           <Button
             variant="outlined"
             className="containerProcessList__button-continue"
-            onClick={() => {}}
+            onClick={handleNextSummary}
           >
             Ver resumen
           </Button>
@@ -99,12 +140,41 @@ export const ProcessList = () => {
           <Button
             variant="outlined"
             className="containerProcessList__button-continue"
-            onClick={() => {}}
+            onClick={handleNextList}
           >
             Siguiente lista
           </Button>
         )}
       </div>
     </div>
+  );
+};
+
+interface OptionsProps {
+  list: IList;
+  selectedCandidate: ICandidate | null;
+  handleSelectCandidate: (candidate: ICandidate) => void;
+}
+
+const Options = ({
+  list,
+  selectedCandidate,
+  handleSelectCandidate,
+}: OptionsProps) => {
+  const { data: candidates } = useGetCandidatesByListIdQuery(list?._id || "");
+  return (
+    <>
+      <div className="containerProcessList__title">{list?.title}</div>
+      <div className="containerProcessList__candidates">
+        {candidates?.map((candidate) => (
+          <ProcessListItem
+            key={candidate?._id}
+            candidate={candidate}
+            isSelected={candidate?._id === selectedCandidate?._id}
+            onSelect={handleSelectCandidate}
+          />
+        ))}
+      </div>
+    </>
   );
 };
